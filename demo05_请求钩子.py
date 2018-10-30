3c79f3ee70617ae910755b9e3f08b4aa23481f59
szkFlask01 <szkFlask01@163.com>
2018-10-30 19:34:35 +0800

    异常捕获errorhandler

---
+++ demo05_请求钩子.py
@@ -0,0 +1,20 @@
+from flask import Flask
+app=Flask(__name__)
+@app.before_first_request
+def before_first_request():
+    print("fjdsajfdl")
+@app.route("/")
+def index():
+    return "hello"
+@app.before_request
+def before_request():
+    print("before request")
+@app.after_request
+def after_request(response):
+    print("after_request")
+    return response
+@app.teardown_request
+def teardown_request(e):
+    print("teardown_request")
+if __name__ == '__main__':
+    app.run()
