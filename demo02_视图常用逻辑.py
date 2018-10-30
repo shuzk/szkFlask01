7baf1365c3cc72bbfad61eaa43ef46a2ab19bead
szkFlask01 <szkFlask01@163.com>
2018-10-30 17:23:21 +0800

    jsonify生成一个json的响应

--- demo02_视图常用逻辑.py
+++ demo02_视图常用逻辑.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app=Flask(__name__)
 @app.route("/")
 def index():
     return "hello"
+@app.route("/demo01")
+def demo1():
+    json_dict={
+        "user_id":111,
+        "user_name":"laowang"
+    }
+    return jsonify(json_dict)
 if __name__ == '__main__':
     app.run()
