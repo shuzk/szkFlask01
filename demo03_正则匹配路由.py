d97de076e1a6dee8f30427e878dcd2f9e9184042
szkFlask01 <szkFlask01@163.com>
2018-10-30 15:27:58 +0800

    正则匹配路由

--- demo03_正则匹配路由.py
+++ demo03_正则匹配路由.py
@@ -1,12 +1,15 @@
 from werkzeug.routing import BaseConverter
 from flask import Flask
-app=Flask(__name__)
 
 class RegexConverter(BaseConverter):
     def __init__(self,url_map,*args):
         super(RegexConverter,self).__init__(url_map)
         self.regex=args[0]
-
+app=Flask(__name__)
+app.url_map.converters['re']=RegexConverter
+@app.route('/user/<re("[0-9]{3}"):user_id>')
+def user_info(user_id):
+    return "user_id=%s"%user_id
 
 @app.route("/")
 def index():
