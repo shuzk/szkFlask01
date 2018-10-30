ebaf635d943bbaf002465e88a991725dc2590523
szkFlask01 <szkFlask01@163.com>
2018-10-30 20:49:53 +0800

    上下文与Flask-Script

---
+++ demo08_Flask-Script.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_script import Manager
+app=Flask(__name__)
+manager=Manager(app)
+@app.route("/")
+def index():
+    return "hello"
+if __name__ == '__main__':
+    # app.run()
+    manager.run()
