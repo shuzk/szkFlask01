e1251936c0d01f1abd5f1ebcd7810a365c5971df
szkFlask01 <szkFlask01@163.com>
2018-10-31 18:52:27 +0800

    复用模板——包含

--- 模块/tdemo04_extends.py
+++ 模块/tdemo04_extends.py
@@ -5,5 +5,8 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     return render_template("temp_demo04_extends_son.html")
+@app.route("/include")
+def include():
+    return render_template("temp_demo05_include.html")
 if __name__ == '__main__':
     app.run()
